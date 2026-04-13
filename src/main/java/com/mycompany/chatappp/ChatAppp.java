9dc5c215694b351ebf6073d097fe79551e5d458e
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-04-13 16:06:04 +0200

    added scanner

--- src/main/java/com/mycompany/chatappp/ChatAppp.java
+++ src/main/java/com/mycompany/chatappp/ChatAppp.java
@@ -3,11 +3,12 @@
  */
 
 package com.mycompany.chatappp;
-
+import java.util.Scanner;
 /**
  *
  * @author Tshireletso
  */
+
 public class ChatAppp {
 
     public static void main(String[] args) {
