478ffa7dc021c75c9dd2dec5b0cabf70972715bd
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-04-13 16:03:30 +0200

    opened new project

---
+++ src/main/java/com/mycompany/chatappp/ChatAppp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatappp;
+
+/**
+ *
+ * @author Tshireletso
+ */
+public class ChatAppp {
+
+    public static void main(String[] args) {
+        
+    }
+}
