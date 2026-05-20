5753623b2616e1d5952ad0ac731a9a4fb03335b2
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-05-20 22:08:27 +0200

    Linked my register method to my main class

---
+++ src/main/java/com/mycompany/chatappp/Message.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatappp;
+import java.util.Scanner;
+/**
+ *
+ * @author Tshireletso
+ */
+
+public class Message {
+}
